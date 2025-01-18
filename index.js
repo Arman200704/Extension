9ecac5e0404d9d984ebc8ed1a1d3c2b2632e2dd8
Arman Muradyan <a.muradyan@inorain.com>
2025-01-18 12:03:40 +0400

    no sandbox environment

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ async function sendRequestToChatGPT(filePath, prompt) {
 async function runLighthouse(url) {
   const chrome = await ChromeLauncher.launch({
     startingUrl: url,
-    chromeFlags: ['--headless', '--disable-gpu']
+    chromeFlags: ['--headless', '--disable-gpu', '--no-sandbox']
   })
 
   console.log(`Chrome debugging port running on ${chrome.port}`);
